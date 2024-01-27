cbcd9153ea7ef5ddbbf1fa1eb046323a11384c55
VitaliyDolceVita <47143915+VitaliyDolceVita@users.noreply.github.com>
2024-01-27 19:03:08 +0200

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,4 +1,4 @@
-import os
+import os  # Імпортуємо необхідні модулі
 import sys
 from colorama import Fore
 
@@ -6,29 +6,34 @@ from colorama import Fore
 def list_files_and_directories(directory, indent=""):
     try:
         with os.scandir(directory) as entries:
-            for entry in entries:
-                if entry.is_dir():
-                    entry.name
-                    print(indent + Fore.MAGENTA + entry.name )
-                    list_files_and_directories(entry.path, indent + "  ")
+            for entry in entries: # Проходимось по елементах 
+                if entry.is_dir():  # Якщо обь'єк є папкою 
+                    print(indent + Fore.MAGENTA + entry.name )  # Виводи відступ, назву папки кольору магента
+                    list_files_and_directories(entry.path, indent + "  ")  # збільшуємо відступ при кожному рекурсивному виклику функції
                 else:
-                    print(indent + Fore.GREEN + entry.name)
-    except FileNotFoundError:
+                    print(indent + Fore.GREEN + entry.name)  # Виводи відступ, назву файла зелеого кольору
+    except FileNotFoundError:  # Обробляємо помилки і красим їх вивід в червоний колір
         print(Fore.RED + f"Директорія {directory} не знайдена.")
     except PermissionError:
         print(Fore.RED + f"Немає доступу до директорії {directory}.")
     except Exception as e:
         print(Fore.RED + f"Сталася помилка: {e}")
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Використання: python Task_3.py /шлях/до/директорії")
-        sys.exit(1)
 
-    directory = sys.argv[1]
-    if not os.path.isdir(directory):
-        print(f"{directory} не є директорією.")
-        sys.exit(1)
+def main():
+    if len(sys.argv) != 2:  # яккщо кількість аргументів командного рядка не дорівнює двом
+        print("Використання: python Task_3.py /шлях/до/директорії")   # Виводим приклад вводу користувачем
+        sys.exit(1)  #  Виходимо з програми з кодом помилки 1
 
-    print(f"Структура директорії {directory}:")
-    list_files_and_directories(directory)
+    directory = sys.argv[1]  #  шлях до директорії з першого аргументу командного рядка зберігаєм в змінній
+    if not os.path.isdir(directory):  #  Провіряємо чи об'єкт папка
+        print(f"{directory} не є директорією.")   #  Повідомляємо користувача 
+        sys.exit(1)  # Вихід з програми з кодом помилки 1
+
+    print(f"Структура директорії {directory}:")  #  Друкуєм структуру папак і файлів
+    list_files_and_directories(directory)   # Викликаэмо  функцію
+
+
+if __name__ == "__main__":  # Перевіряємо чи файл не викликається як модуль
+    main()
+    
