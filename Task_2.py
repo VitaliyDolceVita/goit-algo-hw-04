62f0e9637d15dbb13e9b790b3260608fef635df8
VitaliyDolceVita <47143915+VitaliyDolceVita@users.noreply.github.com>
2024-01-27 17:44:45 +0200

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,17 +1,16 @@
-def get_cats_info(path):
-    cats_info = []
+def get_cats_info(path):  # Визначаємо функцію
+    cats_info = []  # Створюємо пустий список для збереження словників з данними
     try:
         with open(path, 'r', encoding='utf-8') as file:
             for line in file:
-                # Розділяємо рядок на ідентифікатор, ім'я та вік кота
-                cat_data = line.strip().split(',')
-                cat_info = {
+                cat_data = line.strip().split(',')  # Розділяємо рядок на ідентифікатор, ім'я та вік кота
+                cat_info = {  #  Створюємо словник для зберігання данних з відповідними ключами
                     "id": cat_data[0],
                     "name": cat_data[1],
                     "age": cat_data[2]
                 }
-                cats_info.append(cat_info)
-    except FileNotFoundError:
+                cats_info.append(cat_info)  # Додаємо словник до списку
+    except FileNotFoundError:  # Обробляємо винятки, помилки
         print("Файл не знайдено.")
     except Exception as e:
         print(f"Сталася помилка: {e}")
